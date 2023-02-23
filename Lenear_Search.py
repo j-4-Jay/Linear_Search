0dc8fffa0fdd85453d1d0cc8875b4abcbdcd711a
Jayaprakash Pattanaik <jpattanaik1983@gmail.com>
2023-02-23 11:41:14 +0530

    organized in github

---
+++ Lenear_Search.py
@@ -0,0 +1,15 @@
+'''
+Given an array... and a target number to find from array...
+find the index of the target number.
+
+'''
+
+array = [1,2,4,3,5,6,7,8,9,10]
+target = 91
+for i in array:
+
+    if i == target:
+        print(f'The number is Present in the array at the {array.index(i+1)} position')
+        break
+    
+print(f'The number is NOT Present in the array')
